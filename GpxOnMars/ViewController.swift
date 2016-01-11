05b3562580b395e768a50a624f25f00c2514b955
Hran <zhuangluda@gmail.com>
2016-01-11 11:20:25 +0800

    Initial Commit

---
+++ GpxOnMars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GpxOnMars
+//
+//  Created by 庄麓达 on 16/1/11.
+//  Copyright © 2016年 Luda Zhuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
